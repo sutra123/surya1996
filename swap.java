3d3b1c5770536069796c6ccf876501f8b138c97e
surya <pratapsinghsurya551@gmail.com>
2020-06-07 21:13:59 +0530

    initial commit

---
+++ swap.java
@@ -0,0 +1,22 @@
+
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=20;		
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("a :"+ a+"    b :"+ b);
+		
+		int c=20,d=10,t=0;
+		t=c;
+		c=d;
+		d=t;
+		System.out.println("c :"+ c+"    d :"+ d);	
+		
+
+	}
+
+}
